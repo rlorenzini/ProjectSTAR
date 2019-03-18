3753ea2dfb28b2fd5bf976cc3214eaedffd6afbe
Tiffany May-Phillips <47507790+tmayphillips@users.noreply.github.com>
2019-03-18 12:53:01 -0500

    Add files via upload

--- classes.js
+++ classes.js
@@ -1,16 +1,26 @@
 class Article {
-  constructor(key,title,url) {
+  constructor(key,user, title,url) {
     this.key = key
-    this.title = name
+    this.user = user
+    this.title = title
     this.url = url
   }
 }
 
 class Comment {
-  contructor(key,user,article,comment) {
+  constructor(key,user,blog,comment) {
   this.key = key
   this.user = user
-  this.article = article
+  this.blog = blog
   this.comment = comment
   }
+}
+
+class Blog {
+  constructor(key,blogID,title,url) {
+  this.key = key
+  this.blogID = blogID
+  this.title = title
+  this.url = url
+}
 }
--- main.js
+++ main.js
@@ -3,13 +3,15 @@ let myUserId = ''
 let users = []
 let savedArticles = []
 let comments = []
+let blogs = []
 let signInUser = document.getElementById('signInUser')
 let signUpUser = document.getElementById('signUpUser')
 let signOut = document.getElementById('signOut')
 
+
 database.ref("articles")
 .on("child_added",function(snapshot){
-  let article = new Article(snapshot.key,snapshot.val().user,snapshot.val().article)
+  let article = new Article(snapshot.key,snapshot.val().user,snapshot.val().title,snapshot.val().url)
   savedArticles.push(article)
 })
 
@@ -20,6 +22,25 @@ database.ref("articles")
     })
 })
 
+database.ref("blogs")
+.on("child_added",function(snapshot){
+  let blog = new Blog(snapshot.key,snapshot.val().blogID,snapshot.val().title,snapshot.val().url)
+  blogs.push(blog)
+})
+
+database.ref("blogs")
+.on("child_removed",function(snapshot){
+    blogs = blogs.filter((blog) => {
+      return blog.key != snapshot.key
+    })
+})
+
+database.ref("blogs")
+.on("child_removed",function(snapshot){
+    blogs = blogs.filter((blog) => {
+      return blog.key != snapshot.key
+    })
+})
 database.ref("comments")
 .on("child_added",function(snapshot){
   let comment = new Comment(snapshot.key,snapshot.val().user,snapshot.val().article,snapshot.val().comment)
@@ -36,15 +57,28 @@ database.ref("comments")
 signInUser.addEventListener('click',function() {
   let emailAddress = document.getElementById('emailAddress').value
   let password = document.getElementById('password').value
-  firebase.auth().signInWithEmailAndPassword(emailAddress,password)
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      })
   getUID()
+
 })
 
 signUpUser.addEventListener('click',function() {
   let emailAddress = document.getElementById('emailAddress').value
   let password = document.getElementById('password').value
   userName(emailAddress,password)
-  firebase.auth().createUserWithEmailAndPassword(emailAddress,password)
+  firebase.auth().createUserWithEmailAndPassword(emailAddress,password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode == 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(errorMessage);
+      }
+        console.log(error);
+      })
 })
 
 signOut.addEventListener('click',function(){
@@ -54,25 +88,9 @@ signOut.addEventListener('click',function(){
 })
 })
 
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    if (errorCode == 'auth/weak-password') {
-      alert('The password is too weak.');
-    } else {
-      alert(errorMessage);
-    }
-      console.log(error);
-    })
-
-firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    });
-
 firebase.auth().onAuthStateChanged(function(user) {
     window.user = user;
-    });
+    })
 
 function getUID() {
     var user = firebase.auth().currentUser;
@@ -91,22 +109,22 @@ function userName(emailAddress,password) {
       })
   }
 
-function saveArticle (articleTitle, articleURL) {
-  let user = getUID()
+function saveArticle (title,articleURL) {
+  console.log(articleURL);
+  let userID = getUID()
   let articlesRef = database.ref("articles")
   let articleRef = articlesRef.push({
-    user: user,
-    title: articleTitle,
-    url: articleURL
+    user: userID,
+    url: articleURL,
+    title: title
   })
 }
 
-function getSavedArticles () {
-    document.getElementById("articlesUL").style.display = "block";
+function getSavedArticles() {
     let articlesLI = savedArticles.map((article) => {
       if (article.user == getUID()) {
         return `<li>
-            <a src ='${article.url}'>${article.title}></a>
+            <a src ='${article.url}'>${article.title}</a>
             <button onclick="deleteArticle('${article.key}')">Delete</button>
             </li>`
       } else {
@@ -117,24 +135,51 @@ function getSavedArticles () {
 
 function deleteArticle(articleKey) {
   database.ref("articles").child(articleKey).remove()
+  getSavedArticles()
 }
 
 function submitComment (blog, comment) {
   let user = getUID()
-  let commentsRef = database("comments")
+  let commentsRef = database.ref("comments")
   let commentRef = commentsRef.push({
     user: user,
     blog: blog,
     comment: comment
   })
-  let articlesLI = savedArticles.map((article) => {
-    if (article.user == getUID()) {
+  let commentsLI = comments.map((comment) => {
+    if (comment.blog == blogID) {
       return `<li>
-          <a src ='${article.url}'>${article.title}></a>
-          <button onclick="deleteArticle('${article.key}')">Delete</button>
+          ${comment.comment}
           </li>`
     } else {
     }
   })
-  articlesUL.innerHTML = articlesLI.join("")
+  commentsUL.innerHTML = commentsLI.join("")
 }
+
+function addBlog (blogID,title) {
+  let userID = getUID()
+  let blogsRef = database.ref("blogs")
+  let blogRef = blogsRef.push({
+    blogID: blogID,
+    title: title
+  })
+}
+
+console.log("Houston");
+let news = document.getElementById("news")
+let Houston = "https://newsapi.org/v2/everything?q=Houston+Texas=US&sortBy=popularity&apiKey=1a6cdc031db640ff9e62b8e0e7716746"
+fetch(Houston)
+.then(response=>response.json())
+   .then(function(json){ return json
+   })
+   .then (function(json){
+       let newsID = json.articles.map(function(news){
+         let articleTitle = JSON.stringify(news.title).replace(/&/, "&amp;").replace(/"/g, "&quot;").replace(/'/g, "&quot;")
+           return `<li>
+           <a href = '${news.url}'>${news.title}</a>
+           <button onclick="saveArticle('${articleTitle}','${news.url}')">Save</button>
+           </li>`
+       })
+       news.innerHTML=newsID.join("")
+   })
