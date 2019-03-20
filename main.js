91de707cef61093aa693a99d5ecda4a0a6aaaaff
TIFFANY MAY-PHILLIPS <tiffanymay-phillips@TIFFANYs-MBP.localdomain>
2019-03-20 12:12:51 -0500

    blog entry working

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ let news = document.getElementById("news")
 let submitBtn = document.getElementById("submitBtn")
 let zipInput = document.getElementById("zipInput")
 let zippers = 77002
+let imageURL = ""
 
 document.getElementById('blogEntry').style.display = "none";
 
@@ -251,7 +252,6 @@ function blogEntry () {
     document.getElementById('blogEntry').style.display = "block";
     let form = document.createElement("form")
     form.setAttribute('method',"post")
-    // form.setAttribute('action',"submit.php");
 
     let title = document.createElement("input")
     title.setAttribute('type',"text")
@@ -261,6 +261,13 @@ function blogEntry () {
     let image = document.createElement("input")
     image.setAttribute('type',"file")
     image.setAttribute('id', "imageEntry")
+    image.setAttribute('value','upload')
+
+    let progress = document.createElement("progress")
+    progress.setAttribute('value',"0")
+    progress.setAttribute('max',"100")
+    progress.setAttribute('id',"uploader")
+    progress.innerHTML = "0%"
 
     let content = document.createElement("textarea")
     content.setAttribute('cols',"35")
@@ -274,27 +281,53 @@ function blogEntry () {
 
     form.appendChild(title)
     form.appendChild(image)
+    form.appendChild(progress)
     form.appendChild(content)
     form.appendChild(blogEntryBtn)
 
     document.getElementById('blogEntry').appendChild(form);
 
-    blogEntryBtn.addEventListener("click",function(){
-    let userID = getUID()
-    let titleEntry = document.getElementById('titleEntry').value
-    let imageEntry = document.getElementById('imageEntry').value
-    let contentEntry = document.getElementById('contentEntry').value
-    let blogsRef = database.ref("blogs")
-    console.log(titleEntry);
-    let blogRef = blogsRef.push({
-      user: userID,
-      blogID: "blog3",
-      blogTitle: titleEntry,
-      blogImg: imageEntry,
-      blogContent: contentEntry
-    })
-    })
-  }
+
+
+setTimeout(function() {
+  imageEntry.addEventListener('change', function(e) {
+    var file = e.target.files[0]
+    var storageRef = firebase.storage().ref('images/' + file.name)
+    var task = storageRef.put(file)
+    task.on('state_changed',
+      function progress(snapshot) {
+        var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        uploader.value = percentage
+      },
+      function error(err) {
+      },
+      function complete() {
+        var imageBucket = firebase.storage().ref('images/' + file.name);
+        imageBucket.getDownloadURL().then(function(url){
+          imageURL = url
+        })
+      }
+    )
+  })
+},3000)
+
+
+
+blogEntryBtn.addEventListener("click",function(){
+let userID = getUID()
+let titleEntry = document.getElementById('titleEntry').value
+let contentEntry = document.getElementById('contentEntry').value
+let blogsRef = database.ref("blogs")
+console.log(titleEntry);
+let blogRef = blogsRef.push({
+  user: userID,
+  blogID: "blog2",
+  blogTitle: titleEntry,
+  blogImg: imageURL,
+  blogContent: contentEntry
+})
+})
+}
 }
 
 function getBlogs() {
@@ -321,19 +354,6 @@ function displayBlogs() {
     })
     document.getElementById('blog').innerHTML = blogsLI.join("")
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 function getDirection(angle) {
    let directions = ['North', 'North-East', 'East', 'South-East', 'South', 'South-West', 'West', 'North-West'];
--- storage.js
+++ storage.js
@@ -1,15 +1,9 @@
-service firebase.storage {
-  match /b/{bucket}/o {
-    match /{allPaths=**} {
-      allow read, write: if request.auth != null;
-    }
-  }
-}
 
-var ref= firebase.database().ref("Uploads");
-var storage = firebase.storage();
-var pathReference = storage.ref('images/stars.jpg');
-pathReference.getDownloadURL().then(function(url) {
-ref.push().set({
-imgurl: url
-});
+
+// var ref= firebase.database().ref("Uploads");
+// var storage = firebase.storage();
+// var pathReference = storage.ref('images/stars.jpg');
+// pathReference.getDownloadURL().then(function(url) {
+// ref.push().set({
+// imgurl: url
+// });
