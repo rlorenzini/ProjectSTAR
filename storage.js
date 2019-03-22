2248fe6847f98a248ad921e5d2b8a0c7712c849a
Richard Lorenzini <47166320+rlorenzini@users.noreply.github.com>
2019-03-22 14:30:59 -0500

    Delete storage.js

--- storage.js
+++
@@ -1,9 +0,0 @@
-
-
-// var ref= firebase.database().ref("Uploads");
-// var storage = firebase.storage();
-// var pathReference = storage.ref('images/stars.jpg');
-// pathReference.getDownloadURL().then(function(url) {
-// ref.push().set({
-// imgurl: url
-// });
